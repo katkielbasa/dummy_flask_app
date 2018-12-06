13d95f4f53c67d4cec39457996e4613e946a3bc5
Katrzyna Kielbasa <katkielbasa@gmail.com>
2018-12-06 01:54:55 +0100

    add basic html and first route

--- dummy_flask_app/app.py
+++ dummy_flask_app/app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route("/") 
 def hello():
-    return render_template("hello.html")
+    return render_template("cover.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("sign_in.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
